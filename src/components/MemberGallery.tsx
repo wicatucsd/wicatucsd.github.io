0d706081c10bfb280bd9c8a77d829e311e1b9569
jenetic <jznnylam@gmail.com>
2025-04-24 23:36:47 -0700

    Go back to grid

--- src/components/MemberGallery.tsx
+++ src/components/MemberGallery.tsx
@@ -11,9 +11,15 @@ export default function MemberGallery({ members }: MemberGalleryProps) {
   return (
 
    <div className="
-      flex flex-wrap
-      items-start justify-center
-      gap-md px-md md:px-lg lg:px-xl
+      grid
+      grid-cols-2 
+      md:grid-cols-3 
+      lg:grid-cols-4 
+      gap-md
+      px-md
+      md:px-lg
+      lg:px-xl
+      place-items-center items-start
       "
     >
       {members.map((member: { imageSrc: string; name: string; }, index: Key | null | undefined) => (
